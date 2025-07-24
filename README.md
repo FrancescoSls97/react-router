24bfecf71ef77d81ba1af7a55a0e36214be5b532
FrancescoSls97 <frankds1997@gmail.com>
2025-07-24 15:20:11 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# React + Vite
+
+Fake store API: https://fakestoreapi.com/
---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import * as bootstrap from 'booystrap'
+
+createRoot(document.getElementById('root')).render(
+
+    <App />
+
+)
