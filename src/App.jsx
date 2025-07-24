a1a543addbbf0eb4804412bded20e67577167a53
FrancescoSls97 <frankds1997@gmail.com>
2025-07-24 16:00:20 +0200

    add navbar markup

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,27 @@
 import { useState } from 'react'
 
-import './App.css'
 
 function App() {
-
-
   return (
     <>
-
+     <header>
+      <nav className="navbar navbar-expand-sm navbar-light bg-light ">
+        <div className="container p-2">
+          <a className="navbar-brand" href="#">Ciccio Shop</a>
+          <div className="collapse navbar-collapse" id="collapsibleNavId">
+            <ul className="navbar-nav me-auto mt-2 mt-lg-0">
+              <li className="nav-item">
+                <a className="nav-link active" href="#" aria-current="page">Prodotti</a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#">Chi Siamo </a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>  
+     </header>
     </>
   )
 }
-
 export default App
--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,8 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import * as bootstrap from 'booystrap'
+import App from './App.jsx'
+import * as bootstrap from 'bootstrap'
+
 
 createRoot(document.getElementById('root')).render(
 
